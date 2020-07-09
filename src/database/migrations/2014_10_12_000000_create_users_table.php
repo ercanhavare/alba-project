63d5afc0106ef0d8fee87e4b8bf3efbbf685722b
Ercan Havare <ercanhavare@gmail.com>
2020-07-09 22:24:27 +0300

    create database elements, controllers, and models

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,16 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string("mobile");
             $table->string('password');
+            $table->unsignedBigInteger("role_id");
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign("role_id")->references("id")->on("roles")->onDelete("cascade");
         });
     }
 
