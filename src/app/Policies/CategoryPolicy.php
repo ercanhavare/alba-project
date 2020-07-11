01dffcb3d960a9c5c85dc05bd7aa3606bdd0c303
Ercan Havare <ercanhavare@gmail.com>
2020-07-11 12:46:13 +0300

    category policy

---
+++ src/app/Policies/CategoryPolicy.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Category;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CategoryPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  User  $user
+     * @param  Category  $category
+     * @return mixed
+     */
+    public function view(User $user, Category $category)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @param  User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+       return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  User  $user
+     * @param  Category  $category
+     * @return mixed
+     */
+    public function update(User $user, Category $category)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  User  $user
+     * @param  Category  $category
+     * @return mixed
+     */
+    public function delete(User $user, Category $category)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  User  $user
+     * @param  Category  $category
+     * @return mixed
+     */
+    public function restore(User $user, Category $category)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  User  $user
+     * @param  Category  $category
+     * @return mixed
+     */
+    public function forceDelete(User $user, Category $category)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+}
